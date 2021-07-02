1b0a5362ab059699c3468f4aedebf9c7d3e908d5
Abhay Pratap Singh <74309121+AbhayPratap007@users.noreply.github.com>
2021-07-02 19:27:31 +0530

    Add files via upload

---
+++ GUVI Python program.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 22 08:40:21 2021
+
+@author: Abhay
+"""
+
+Mycar ="Maruti Suzuki Nexa Baleno Alpha"
+print(Mycar)
+
+year = 2021
+print(year)
+
+Chemistry = 78
+Math = 80
+Physics = 85
+English = 58
+total = Chemistry+Math+Physics+English
+print(total)
+
+#
+Yourspeed = 100
+speedlimit = 40
+if Yourspeed>speedlimit:
+    print("you are going in speed limit") #if the condition is true then the result will print otherwise not. 
+    
+else:
+   print("you are trading in safe speed")#print what condition is true
+    
+elif Yourspeed==speedlimit:
+    print("you are traveling in the exact speed limit")
+    
+#for chat
+
+if Internet is there and sent:
+    print("single_tick")
+if sent and delivered:
+    print("double_tick")
+if seen and (userpref==show):
+    print("double_tick_with_color")
+
+friend1="Danielle Campbell"
+friend2="Melissa Benoist"
+friend3="scarlet johans"
+friend4="katherine langford"
+friend5="Catherine Tresa Alexander"
+print(friend1,friend2,friend3,friend4,friend5)
+
+myfriend=["Danielle Campbell","Melissa Benoist","scarlet johans","katherine langford","Catherine Tresa Alexander"]
+#print(my_friend[2])
+    
+if "Catherine Tresa Alexander" is my friend:
+    print("Catherine Tresa Alexander is myfriend list")
+else:
+    print("Catherine Tresa Alexander is not myfriend list")
+
+#loop:- When you want to repeat a block of code a number of times.
+current_lift_floor = 0
+lift_button_pressed = 5
+while current_lift_floor = lift_button_pressed:
+    print(current_liftO_button)
+    current_lift_floor = current_lift_floor + 1
+    
+print(current_lift_floor)
+print("you have reached over floor!")    
+
+myfriend=["Danielle Campbell","Melissa Benoist","scarlet johans","katherine langford","Catherine Tresa Alexander"]
+print("my friends are", myfriend)
+for item in myfriend:
+    print(item[0])
+    if item[0] in "e";
+    print(item)
+
+#for loop is basically used to iterate over a sequence.    
+x = "fruits"
+for a in x:
+    print(a)
+
+for x in range(5):
+    print(x)
+for x in range(6,10):
+    print(x)
+    
+#break statement
+numbers =[1,2,3,4,5,6]
+for x in numbers:
+    print(x)
+    if x==3:
+        break
+#continue statement
+numbers =[1,2,3,4,5,6]
+for x in numbers:
+    print(x)
+    if x==4:
+        continue
+    print(x)
+#while loop will let you execute a set of statement while it is true.
+ i=1
+ while i<9:
+     print(i)
+     i +=1
+     
+i=1
+while i<5:
+    print(i)
+    if i==3:
+        break
+    i +=1
