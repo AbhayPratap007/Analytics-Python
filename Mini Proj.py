8a2e78f7429a4b553aa4edc3b7d36ca7451db299
Abhay Pratap Singh <74309121+AbhayPratap007@users.noreply.github.com>
2021-09-22 10:13:56 -0700

    Add files via upload

---
+++ Mini Proj.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 22 21:44:43 2021
+
+@author: Singh
+"""
+
+#create imojies
+print("\U0001F600")
+print("\U0001F618")
+print("\U0001F637")
+print("\U0001F917")
+print("\U0001F62A")
