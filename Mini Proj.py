458de20b64788f955ad170ace4012c2dd2dd53f1
Abhay Pratap Singh <74309121+AbhayPratap007@users.noreply.github.com>
2021-09-26 20:17:30 -0700

    Update Mini Proj.py

--- Mini Proj.py
+++ Mini Proj.py
@@ -18,3 +18,39 @@ for i in range(rows+1,0,-1):
   for j in range(0,i-1):
     print("*",end=" ")
   print(" ")
+
+ #print pattern
+n = 6
+for i in range(0,n):
+    for j in range(0,i+1):
+        print("*",end='')
+    print()
+    
+#Number Pattern
+n = 5
+for i in range(n+1):
+    for j in range(i):
+            print(i,end='')
+    print()
+    
+#Half pyramid pattern with the number
+n = 5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
+
+#Multiplication Number in column
+n = int(input("Enter the number of rows"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i*j,end='')
+    print()
+    
+#Don't do this way
+name = "Abhay !!"
+print("Hello, my name is",name)
+#Do this way
+name = "Abhay !!"
+print(f"Hello, my name is {name}")
+ 
