b2784f4f7d7dab4908981a476598c19a2a253811
Abhay Pratap Singh <74309121+AbhayPratap007@users.noreply.github.com>
2022-03-01 12:02:17 +0530

    Update Function_basics.py

--- Function_basics.py
+++ Function_basics.py
@@ -140,3 +140,8 @@ List = [1,2,3,4,10,123,22]
 new_list = list(map(lambda x:x*3,List)) 
 # this will return the triple of each item of the list and add it to new_list  
 print(new_list)  
+
+#def function
+def my_function(x):
+  return 5 * x
+print (my_function(3))
