d691c9a2c7602303ac56f7af8223973e01d73963
Abhay Pratap Singh <74309121+AbhayPratap007@users.noreply.github.com>
2022-10-20 13:24:59 +0530

    Update Data-Structures.py

--- Data-Structures.py
+++ Data-Structures.py
@@ -10,9 +10,9 @@ print(x)
 #A value(10) is a data type,the Data-type is basically the type od value that
 #you assign to the variable.
 
-print("hello world")
-print("hello \nworld")
-print("hello \tworld")
+print("hello world")  
+print("hello \nworld")  #print in next line
+print("hello \tworld")  #print with tab
 
 name = ''' Hey there, #triple codes using forSpanning strings over multiple lines.
 Abhay this side !!''' 
